b4044bd26267a88b506c9f2b9ec9e936a0e45545
faguirre <faguirre@alumnos.inf.utfsm.cl>
2020-11-13 13:00:10 -0300

    Arreglado el famoso Chihau

--- Ejemplo React Native/App.js
+++ Ejemplo React Native/App.js
@@ -1,67 +1,50 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, SafeAreaView, Image, TouchableOpacity, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react'
+import { SafeAreaView, Text, Image, StyleSheet, Button, TouchableOpacity } from 'react-native'
+
+import Card from "./Card.js"
 
 
-
-function HomeScreen({ navigation }) {
-  const [a, setA] = useState(0);
-  const [nombre, setNombre] = useState("Juanito")
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button title="Boton" onPress={() => {
-        setNombre("Felipe")
-        console.log(a)
-        setA(a + 1)
-      }
-      }></Button>
-
-      <Text>{nombre}</Text>
-    </SafeAreaView >
-  );
-}
-const Stack = createStackNavigator();
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{
-          headerShown: false
-        }}
-          name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-export default App;
+    <SafeAreaView style={estilo.bloquePrincipal}>
+      <TouchableOpacity>
+        <Image style={estilo.avatar} source={require('./assets/avatar.jpg')} ></Image>
+      </TouchableOpacity>
+      <Text style={estilo.textTitulo}>Felipe Aguirre</Text>
+      <Text style={estilo.textTitulo}>Ayudante</Text>
 
-const styles = StyleSheet.create({
-  container: {
+      <Card texto="+56 9 1234 5678" icono="phone" />
+      <Card texto="faguirre@alumnos.inf.utfsm.cl" icono="email" />
+      <Card texto="Español / Inglés" icono="language" />
+      <Card texto="Av. Siempre Viva 746" icono="directions" />
+
+      <Card texto="React JS/ React Native" icono="code" />
+
+
+    </SafeAreaView>)
+
+
+}
+
+const estilo = StyleSheet.create({
+  bloquePrincipal: {
     flex: 1,
     backgroundColor: 'rgb(236,241,248)',
     alignItems: 'center',
     justifyContent: 'center'
-
-  },
-  image: {
-    width: "90%",
-    height: "90%",
-    resizeMode: "contain",
-    alignSelf: "center",
-
   },
   avatar: {
     width: 120,
     height: 120,
-    borderRadius: 400 / 2,
+    borderRadius: 120 / 2,
     margin: 20,
     borderWidth: 2,
-    borderColor: "#CACACA"
+    borderColor: '#CACACA'
   },
   textTitulo: {
     color: "rgb(64,65,79)",
     fontSize: 30
   }
 })
+
+export default App
